3843518a06ef820b5a75572997739eb46fad4596
Bao0910 <Baonguyen091099@gmail.com>
2020-09-28 15:41:08 +0700

    initial project

---
+++ content.js
@@ -0,0 +1,7 @@
+document.getElementById("content").style.backgroundColor = "#ddd";
+// document.getElementById("content").style.backgroundImage = "url('/background.jpg')";
+//document.getElementById("leftcontent").style.backgroundImage = "url('https://www.onlygfx.com/wp-content/uploads/2019/09/8-colorful-splatter-frame-1.png')";
+//document.getElementById("rightcontent").style.backgroundImage = "url('https://lh3.googleusercontent.com/proxy/L3u5pdfz57adkDrMeX8jgdbSHOT4WnALKHIiUgcvK7eiqHAcWQ1AODCi5u9I1VHBqzNHp3-OxAis0vuYDk4oxPo9F9eukk1x9Dtb6bpDcbHF_sgh3cEwgO_qKRjCITunJ6FkvhPW')";
+// document.getElementById("leftcontent").style.width = "100%";
+// document.getElementById("leftcontent").style.marginTop = "100px";
+document.getElementById("banner").style.backgroundColor = "brown";
